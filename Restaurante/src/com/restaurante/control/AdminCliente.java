c23d996498db7fba2d6db9b5ae1343c903272fba
Arnaldo Andrés Barrios Mena <cyberarnaldo04@hotmail.com>
2024-01-19 10:43:29 -0500

    Reestructuración de Proyecto
    Reestructuración de Proyecto.

--- Restaurante/src/com/restaurante/control/AdminCliente.java
+++ Restaurante/src/com/restaurante/control/AdminCliente.java
@@ -1,4 +1,9 @@
 /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -34,10 +39,14 @@ public class AdminCliente {
     public boolean estaVacia() {
         return this.indice == -1;
     }
-
-    public void registrar(Cliente cliente) {
-        this.listadoClientes[this.indice] = cliente;
-        this.indice++;
+    
+    public boolean registrar(Cliente cliente) {
+        boolean existe = this.existe(cliente.getIdentificacion());
+        if (!existe) {
+            this.listadoClientes[this.indice] = cliente;
+            this.indice++;
+        }
+        return existe;
     }
 
     public boolean existe(String identificacion) {
